4af58735b2dce84bc116114200015034aa1ca9d0
63070028 <71110346+63070028@users.noreply.github.com>
2022-10-04 10:48:38 +0700

    Add files via upload

---
+++ discovery-server/src/test/java/com/sop/lab8/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sop.lab8.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
